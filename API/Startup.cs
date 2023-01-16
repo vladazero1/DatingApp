829d9e8fb94ad3d1d337bbc621a30927c65c71eb
Vlada <vladazero1@gmail.com>
2023-01-16 13:30:56 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
             });
         
             services.AddControllers();
+            services.AddCors(); 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -60,6 +61,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x =>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
