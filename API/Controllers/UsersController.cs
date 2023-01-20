4338057a39b2fa7e92334781e464f0a1e5cbc137
Vlada <vladazero1@gmail.com>
2023-01-20 15:20:46 +0100

    End of section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -18,6 +18,8 @@ namespace API.Controllers
         {
             _context = context;
         }
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating app';
-  users: any;
+ 
+  constructor(private accountService: AccountService){}
 
-  constructor(private http: HttpClient){}
 
-
-  ngOnInit() {
-    this.getUsers();
+  ngOnInit(): void {
+    this.setCurrentUser();
   }
   
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response=>{
-   this.users = response;
-  }, error =>{
-    console.log(error);
-   
-   })
+  setCurrentUser () {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
