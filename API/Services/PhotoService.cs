4c203e1f35cfacc4c16524ca92550ab81903ac5b
Vlada <vladazero1@gmail.com>
2023-05-02 23:30:03 +0200

    End of section 11

--- API/Services/PhotoService.cs
+++ API/Services/PhotoService.cs
@@ -9,7 +9,7 @@ namespace API.Services
   
     public class PhotoService : IPhotoService
     {
-          private readonly Cloudinary _cloudinary;
+        private readonly Cloudinary _cloudinary;
         public PhotoService(IOptions<CloudinarySettings> config)
         {
             var acc = new Account 
